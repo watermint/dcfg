e39f84600be976e7eb844ec0846c282570004b83
takayuki <watermint@users.noreply.github.com>
2018-04-05 14:48:45 +0900

    fix typo

--- build/build_inside_docker.sh
+++ build/build_inside_docker.sh
@@ -22,9 +22,9 @@ fi
 
 LD_FLAGS="-X main.AppVersion=$APP_VERSION"
 
-GOOS=windows GOARCH=386   go build --ldflags "$LD_FLAGS" -o $BUILD/tbx-$APP_VERSION-win.exe github.com/watermint/dcfg
-GOOS=linux   GOARCH=386   go build --ldflags "$LD_FLAGS" -o $BUILD/tbx-$APP_VERSION-linux   github.com/watermint/dcfg
-GOOS=darwin  GOARCH=amd64 go build --ldflags "$LD_FLAGS" -o $BUILD/tbx-$APP_VERSION-darwin  github.com/watermint/dcfg
+GOOS=windows GOARCH=386   go build --ldflags "$LD_FLAGS" -o $BUILD/dcfg-$APP_VERSION-win.exe github.com/watermint/dcfg
+GOOS=linux   GOARCH=386   go build --ldflags "$LD_FLAGS" -o $BUILD/dcfg-$APP_VERSION-linux   github.com/watermint/dcfg
+GOOS=darwin  GOARCH=amd64 go build --ldflags "$LD_FLAGS" -o $BUILD/dcfg-$APP_VERSION-darwin  github.com/watermint/dcfg
 
 cd $BUILD
 zip -9 -r $DIST/dcfg-$APP_VERSION.zip .
